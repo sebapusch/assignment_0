f2a25f167930ac7fae7536e7c7919eb78acd7d67
atodorov284 <atodorov284@gmail.com>
2025-08-23 17:57:49 +0200

    Revert "switch to ty"
    This reverts commit bc7fa7cdceea06dd3c20ed689e779ee7ed3d341e.

--- main.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-def add_two_integer_lists(a: list[int], b: list[str]) -> int:
-    arr=np.array(a)+np.array(b)
-    print(f"My added array: {arr}")
-if __name__ == "__main__":
-    add_two_integer_lists([1,2,3],[4,5,6])
